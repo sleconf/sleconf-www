8560ff36d8d3ba0f4ba14daca8bb39c11611b655
Eric Van Wyk <evw@cs.umn.edu>
2014-04-15 11:17:06 -0500

    Adding a page pointing to the LNCS proceedings, trying again

---
+++ 2013/Proceedings.ml
@@ -0,0 +1,8 @@
+---
+title: Proceedings
+layout: default2013
+---
+
+6th International Conference, SLE 2013, Indianapolis, IN, USA, October 26-28, 2013. Proceedings, Lecture Notes in Computer Science, Vol. 8225
+Erwig, Martin, Paige, Richard, Van Wyk, Eric (Eds.)
+http://www.springer.com/computer/swe/book/978-3-319-02653-4.
